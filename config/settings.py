2512d09fd80be9ef3b5421273ad187cb6aaa18e7
waqas-ayub <waqasayub002@gmail.com>
2026-09-08 12:30:58 +0500

    Add Celery broker and result backend settings

--- config/settings.py
+++ config/settings.py
@@ -138,3 +138,8 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     )
 }
+
+CELERY_BROKER_URL = os.getenv('REDIS_URL', os.getenv('CELERY_BROKER_URL', 'redis://127.0.0.1:6379/0'))
+CELERY_RESULT_BACKEND = os.getenv('REDIS_URL', os.getenv('CELERY_RESULT_BACKEND', 'redis://127.0.0.1:6379/0'))
+
+
